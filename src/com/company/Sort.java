4c847c39b80b626c812d551ef68dd51aa6c66f8e
pwillsmaith <pwillsmaith@gmail.com>
2020-12-06 22:01:19 -0500

    commit change

--- src/com/company/Sort.java
+++ src/com/company/Sort.java
@@ -1,15 +1,40 @@
 package com.company;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Sort extends JFrame {
-    public Sort(){
+    static final int WIDTH = 450;                // frame width
+    static final int HEIGHT = 300;               // frame height
+
+   /* public Sort(){
         setTitle("Sort list ");
 
+    }*/
+    public static <type extends Comparable<type>> type[] Sorting(type[] list, type temp){
+        int size = list.length;
+        for( int i = 1 ; i < size; ++i){
+            int j = i-1;
+            type key = list[i];
+            while ( j >= 0 && key.compareTo( list[j] ) < 0) {
+                temp =  key;
+                list[j+1] = list[j];
+                j --;
+            }
+            list[j+1] = key;
+        }
+        return list;
+    }
+
+    @Override
+    public String toString() {
+        return "Sort{}";
     }
 
     public static void main(String[] args) {
-        // write your code here
+        Integer[] number = new Integer[]{9,5,1,3,2};
+        int num = 0;
+        for ( int x : Sorting(number, num)){
+            System.out.print(x);
+        };
     }
 }
