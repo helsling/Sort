dbb922c0cee21e31d3ee78f44d84f182336a1159
pwillsmaith <pwillsmaith@gmail.com>
2020-12-06 21:55:26 -0500

    implement insertion sort

---
+++ src/com/company/Sort.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Sort extends JFrame {
+    public Sort(){
+        setTitle("Sort list ");
+
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
